4a59e7e7fa23d6b27fbab1aebe21ecd2d82fe916
Percy <percival.ngan@gmail.com>
2019-12-24 17:27:37 -0600

    Set constraints in view

--- Stars/Stars/StarsViewController.swift
+++ Stars/Stars/StarsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StarsViewController.swift
 //  Stars
 //
 //  Created by Percy Ngan on 12/24/19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StarsViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
