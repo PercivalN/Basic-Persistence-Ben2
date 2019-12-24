b6f4f7256fb7d1c4d9e771f06a0abdc61e51a085
Percy <percival.ngan@gmail.com>
2019-12-24 17:01:30 -0600

    Init a new project

---
+++ Stars/Stars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stars
+//
+//  Created by Percy Ngan on 12/24/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
