171a383cf1f28138b6ba69f9b8434bdd30732dbd
Percy <percival.ngan@gmail.com>
2019-12-24 23:56:59 -0600

    Connected outlets and actions to class

--- Stars/Stars/StarsViewController.swift
+++ Stars/Stars/StarsViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class StarsViewController: UIViewController {
 
+	// MARK: - Outlets
+	@IBOutlet weak var starNameTextField: UITextField!
+	@IBOutlet weak var distanceTextField: UITextField!
+	@IBOutlet weak var tableView: UITableView!
+
 	override func viewDidLoad() {
+
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+	}
+
+	// MARK: - Actions
+	@IBAction func printStarsButtonTapped(_ sender: UIButton) {
+	}
+	@IBAction func createButtonTapped(_ sender: UIButton) {
 	}
 
 
